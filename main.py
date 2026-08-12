e5959cfb7015a85abf047305c25e1a2abb4dd1bc
今村拓哉 <tak@imamuratakuyanoMac-mini.local>
2026-08-12 16:00:10 +0900

    0812FastAPI基本

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def read_root():
 @app.get("/get_name")
 def get_name(name: str = Query(...)):
     return {"name": name}
+
+@app.get("/get_profile")
+def get_profile(age: int = Query(None), name: str = Query(...)):
+    return {"age: ": age, "name: ": name}
+
+@app.get("/get_profile/{name2}/myname")
+def get_p(
+    name2: str,
+    name: str = Query(...),
+    age: int | None = Query(None)
+):
+    return {"name: ": name, "name2: ": name2, "age: ": age}
