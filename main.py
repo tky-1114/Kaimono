5e644b599bafa1d52d6ddacb8d9af8b5dfc5cb22
今村拓哉 <tak@imamuratakuyanoMac-mini.local>
2026-08-15 00:26:00 +0900

    0815CSS連携完了

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Query, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import asyncio
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 templates = Jinja2Templates(directory="templates")
 
