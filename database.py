0a46066564dee523c7f015eafff8dc117e731d03
今村拓哉 <tak@imamuratakuyanoMac-mini.local>
2026-08-24 23:41:21 +0900

    0824.envファイル作成

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
 def connect_mysql():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="practice"
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        database = os.getenv("DB_NAME")
     )
     return conn
 
